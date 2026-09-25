dddd036cc4bc1284de5623d629e7ee93cfe2f178
Ivan <ivan-my-mail@yandex.com>
2026-09-25 17:30:09 +0300

    Exporte[R] (lol)

--- OCGUI_ExportPasswords.bat
+++ OCGUI_ExportPasswords.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-title OpenConnect-GUI Passwords Exported [by Ivan_Alone]
+title OpenConnect-GUI Passwords Exporter [by Ivan_Alone]
 powershell -Command "Set-ExecutionPolicy bypass -scope process -force ; .\OpenConnectGUI-Passwords-Decryptor.ps1"
 pause
